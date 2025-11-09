78f568fac5ba5f9fc06b97b750b3be16c1caf641
sm-remal <remal.mozzem@gmail.com>
2025-11-09 19:59:21 +0600

    filter category bills from mongodb

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 3000;
 
 
 
-app.use(cors());            
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
 // Root route for testing server
 app.get("/", (req, res) => {
-    res.send("Utility Pay Server is running");
+  res.send("Utility Pay Server is running");
 })
 
 
@@ -30,44 +30,58 @@ async function run() {
     await client.connect();
 
     // Define Database and Collections
-        const Database = client.db("utility-pay");
-        const payBillsCollection = Database.collection("bills");
+    const Database = client.db("utility-pay");
+    const payBillsCollection = Database.collection("bills");
 
-        // Get: All Bills
-        app.get("/bills", async (req, res) => {
-            const query = {}
-            const cursor = payBillsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+    // Get: All Bills
+    // app.get("/bills", async (req, res) => {
+    //   const query = {}
+    //   const cursor = payBillsCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
 
-        // GET: Get latest 6 Bills (sorted by date)
-        app.get("/latest-bills", async (req, res) => {
-            const cursor = payBillsCollection.find().sort({ date: -1 }).limit(6);
-            const result = await cursor.toArray();
-            res.send(result);
-        });
+    // GET: Get latest 6 Bills (sorted by date)
+    app.get("/latest-bills", async (req, res) => {
+      const cursor = payBillsCollection.find().sort({ date: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
 
-         // GET: Get a specific product by ID
-        app.get("/bills/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }; 
-            const result = await payBillsCollection.findOne(query);
-            res.send(result);
-        });
+    // GET: Get a specific product by ID
+    app.get("/bills/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await payBillsCollection.findOne(query);
+      res.send(result);
+    });
 
-         // POST: Add a new product
-        app.post("/bills", async (req, res) => {
-            const newBills = req.body;
-            const result = await payBillsCollection.insertOne(newBills);
-            res.send(result);
-        });
-   
+
+    // GET: Get specific category
+    app.get("/bill-category", async (req, res) => {
+      const category = req.query.category;
+      let query = {};
+
+      if (category) {
+        query.category = category;
+      }
+      const result = await payBillsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+    // POST: Add a new product
+    app.post("/bills", async (req, res) => {
+      const newBills = req.body;
+      const result = await payBillsCollection.insertOne(newBills);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
     // await client.close();
   }
 }
@@ -75,5 +89,5 @@ run().catch(console.dir);
 
 // Server listen
 app.listen(port, () => {
-    console.log(`Utility Pay Server at port: ${port}`)
+  console.log(`Utility Pay Server at port: ${port}`)
 });
