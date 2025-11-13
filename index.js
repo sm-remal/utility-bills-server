9a333660f89e22ae6d4011b10b1a93ba9518b249
sm-remal <remal.mozzem@gmail.com>
2025-11-13 14:28:46 +0600

    added encode.js file

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-const serviceAccount = require("./utility-bills-firebase-adminsdk.json");
+// const serviceAccount = require("./utility-bills-firebase-adminsdk.json");
+
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
